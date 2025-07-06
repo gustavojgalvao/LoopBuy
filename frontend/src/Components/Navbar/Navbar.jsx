53787ca2fc83c70941c6f064f5ed94b785bc8646
Gustavo Galvão <gustavojezler@gmail.com>
2025-07-06 14:37:13 -0300

    Update Navbar
    Criação da Navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
+
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className='navbar'>
-        
+      <div className='nav-logo'>
+        <img src={logo} alt="Logo" />
+        <p>LoopBuy</p>
+      </div>
+
+      <ul className="nav-menu">
+        <li onClick={() => setMenu("shop")}>
+          Shop
+          {menu === "shop" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("mens")}>
+          Homens
+          {menu === "mens" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("womens")}>
+          Mulheres
+          {menu === "womens" ? <hr /> : null}
+        </li>
+        <li onClick={() => setMenu("kids")}>
+          Crianças
+          {menu === "kids" ? <hr /> : null}
+        </li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="Carrinho" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
-  )
-}
+  );
+};
